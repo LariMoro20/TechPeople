57c1ce35380217c8d6602afd0b76b0447ea9064b
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2026-06-02 16:53:29 -0300

    feat: cria estrutura inicial e pagina

---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Olá</div>
+</template>
+
+<script setup lang="ts">
+useSeoMeta({
+  titleTemplate: "%s",
+  title: "TechPeople — Catálogo de Profissionais de Tecnologia",
+  description:
+    "Catálogo fictício de profissionais de tecnologia com perfis detalhados, especialidades, localização, senioridade, stacks técnicas e disponibilidade para projetos.",
+  ogTitle: "TechPeople — Catálogo de Profissionais de Tecnologia",
+  ogDescription:
+    "Explore uma listagem fictícia de profissionais de tecnologia, incluindo desenvolvedores frontend, backend, fullstack, UI/UX designers, DevOps e especialistas em dados.",
+});
+</script>
