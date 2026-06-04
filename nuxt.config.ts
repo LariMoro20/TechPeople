aa8264f84a4e3ad3ea768d4cb5956bf15787b84e
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2026-06-03 21:57:30 -0300

    feat: adiciona configurações de SEO, favicon, colorMode e meta tags no nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,4 +20,52 @@ export default defineNuxtConfig({
   experimental: {
     payloadExtraction: false,
   },
+  colorMode: {
+    preference: "light",
+    fallback: "light",
+    storageKey: "techpeople-color-mode",
+  },
+
+  ssr: true,
+
+  ui: {
+    colorMode: true,
+  },
+  app: {
+    head: {
+      titleTemplate: "%s — TechPeople",
+      htmlAttrs: { lang: "pt-BR" },
+      meta: [
+        { name: "theme-color", content: "#06ad5e" },
+        { name: "robots", content: "index, follow" },
+        { name: "author", content: "Larissa Santos" },
+        {
+          name: "keywords",
+          content:
+            "profissionais de tecnologia, desenvolvedores, frontend, backend, fullstack, UI/UX, DevOps, catálogo de profissionais, tech, freelancers",
+        },
+      ],
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        { rel: "manifest", href: "/site.webmanifest" },
+      ],
+    },
+  },
 });
